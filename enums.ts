78c35ae97435b9c748bae5fd2c470e2012fe4c23
Robert Bilsland <RBilsland@users.noreply.github.com>
2025-11-22 15:36:00 +0000

    Update enums.ts

--- enums.ts
+++ enums.ts
@@ -1,10 +1,13 @@
 namespace mesh {
     export enum PacketType {
         Data = 0,
-        Ack = 1
+        Ack = 1,
+        Hello = 2,    // Discovery Request
+        HelloAck = 3  // Discovery Reply
     }
     export enum PayloadType {
         String = 0,
-        Number = 1
+        Number = 1,
+        None = 2
     }
 }
